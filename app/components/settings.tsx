4d05737218cff1a1ca748285a62f06a7395aeae2
Peng <128573629+peng2132@users.noreply.github.com>
2023-11-30 18:12:04 +0800

    Update settings.tsx

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -192,6 +192,7 @@ export function Settings() {
   const updateStore = useUpdateStore();
   const [checkingUpdate, setCheckingUpdate] = useState(false);
   const currentVersion = updateStore.version;
+  
   const remoteId = updateStore.remoteVersion;
   const hasNewVersion = currentVersion !== remoteId;
 
@@ -325,31 +326,7 @@ export function Settings() {
               </div>
             </Popover>
           </SettingItem>
-
-          <SettingItem
-            title={Locale.Settings.Update.Version(currentVersion ?? "unknown")}
-            subTitle={
-              checkingUpdate
-                ? Locale.Settings.Update.IsChecking
-                : hasNewVersion
-                ? Locale.Settings.Update.FoundUpdate(remoteId ?? "ERROR")
-                : Locale.Settings.Update.IsLatest
-            }
-          >
-            {checkingUpdate ? (
-              <div />
-            ) : hasNewVersion ? (
-              <Link href={UPDATE_URL} target="_blank" className="link">
-                {Locale.Settings.Update.GoToUpdate}
-              </Link>
-            ) : (
-              <IconButton
-                icon={<ResetIcon></ResetIcon>}
-                text={Locale.Settings.Update.CheckUpdate}
-                onClick={() => checkUpdate(true)}
-              />
-            )}
-          </SettingItem>
+          
 
           <SettingItem title={Locale.Settings.SendKey}>
             <select
